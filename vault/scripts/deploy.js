93c57d76c329d2fc8b126d283da84c8f75ff2b7a
risingstar2018 <33366277+cryptoinhero@users.noreply.github.com>
2021-09-21 23:28:56 +0800

    add deploying script for vault

--- vault/scripts/deploy.js
+++ vault/scripts/deploy.js
@@ -12,7 +12,63 @@ async function main () {
   const signer = (await ethers.getSigners())[0]
   console.log('signer:', await signer.getAddress())
 
+
+  const config = {
+    vaultChef: true,
+    strategyShiba: true,
+    strategyMasterchef: false,
+  }
+
+  let vaultChefAddress = null
+
+  /**
+   * deploy vaultchef
+   */
+
+  if(config.vaultChef) {
+    const VaultChef = await await ethers.getContractFactory('VaultChef', {
+      signer: (await ethers.getSigners())[0]
+    })
+
+    const vaultchef = await VaultChef.deploy()
+    await vaultchef.deployed()
+
+    console.log('VaultChef deployed to ', vaultchef.address)
+    vaultChefAddress = vaultchef.address
+
+    await sleep(60);
+    await hre.run("verify:verify", {
+      address: vaultchef.address,
+      contract: "contracts/VaultChef.sol:VaultChef",
+      constructorArguments: [],
+    })
+
+    console.log('VaultChef verified')
+  }
   
+  if(config.strategyMasterchef) {
+    const StrategyShiba = await await ethers.getContractFactory('StrategyShiba', {
+      signer: (await ethers.getSigners())[0]
+    })
+
+    const strategyShiba = await StrategyShiba.deploy(vaultChefAddress)
+    await strategyShiba.deployed()
+
+    console.log('StrategyShiba deployed to ', strategyShiba.address)
+
+    await sleep(60);
+    await hre.run("verify:verify", {
+      address: strategyShiba.address,
+      contract: "contracts/StrategyShiba.sol:StrategyShiba",
+      constructorArguments: [vaultChefAddress],
+    })
+
+    console.log('StrategyShiba verified')
+  }
+
+  if(config.strategyMasterchef) {
+
+  }
 }
 
 main()
